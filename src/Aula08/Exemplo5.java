4c7e8eec3239ca7ad99e29aeaab865ba92261c90
AnaRigobelloDEV <anacrigobello000@gmail.com>
2024-10-10 17:13:32 -0300

    Update Exemplo5.java

--- src/Aula08/Exemplo5.java
+++ src/Aula08/Exemplo5.java
@@ -1,8 +1,19 @@
-package Aula08;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Exemplo5 {
     public static void main(String[] args) {
-
-
+        File arquivo = new File("src\\arquivo.txt");
+        if (arquivo.exists()){
+            try {
+                FileWriter fileWriter = new FileWriter(arquivo);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write("\nHAHAHAHHAHAHAHAHHAHAHAHAHHAHAHHA");
+                bufferedWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
